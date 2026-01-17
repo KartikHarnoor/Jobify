8e95275e1f6013a60c0733fd22b756b1c686c501
Kartik Harnoor <kartikpharnoor@gmail.com>
2026-01-17 12:14:41 +0530

    Login and Sigin Page

---
+++ jobify-front-end/next-app/app/login-page/login-page.tsx
@@ -0,0 +1,12 @@
+
+"use-client";
+import React from "react";
+import LoginCard from "./login-card";
+
+export default function LoginPage() {
+    return (
+        <div className ="flex items-center justify-center w-full h-screen bg-gradient-to-b from-[#16303d] to-[#11feb6]">
+            <LoginCard />
+        </div>
+    )
+}
---
+++ jobify-front-end/next-app/app/signin-page/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SigninCard from "./signin-card";
+
+export default function SigninPage(){
+    return (
+        <div className="flex items-center justify-center w-full h-screen bg-gradient-to-b from-[#16303d] to-[#11feb6]">
+            <SigninCard />
+        </div>
+    )
+}
